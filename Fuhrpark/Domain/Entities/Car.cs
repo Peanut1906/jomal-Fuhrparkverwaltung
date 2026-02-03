d5cac901a7f340d7ab1f71c2bae4752e0423789f
Peanut <94468372+Peanut1906@users.noreply.github.com>
2026-02-03 16:15:36 +0100

    Add purchaseValue parameter to Car constructor

--- Fuhrpark/Domain/Entities/Car.cs
+++ Fuhrpark/Domain/Entities/Car.cs
@@ -8,8 +8,8 @@ public sealed class Car : Vehicle
     public int Seats { get; }
     public override VehicleType Type => VehicleType.Car;
 
-    public Car(Guid id, string licensePlate, string brand, string model, int year, int seats)
-        : base(id, licensePlate, brand, model, year)
+    public Car(Guid id, string licensePlate, string brand, string model, int year, int seats, decimal purchaseValue)
+        : base(id, licensePlate, brand, model, year, purchaseValue)
     {
         Seats = Guard.InRange(seats, 1, 9, nameof(seats));
     }
